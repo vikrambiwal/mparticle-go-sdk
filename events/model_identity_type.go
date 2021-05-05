413b4a4c40469447f4f0344b13d1bec0f846c151
Brandon Stalnaker <33703490+BrandonStalnaker@users.noreply.github.com>
2021-05-05 09:32:32 -0400

    Add New Identities

--- events/model_identity_type.go
+++ events/model_identity_type.go
@@ -18,4 +18,13 @@ const (
 	OtherID2                 IdentityType = "other_id_2"
 	OtherID3                 IdentityType = "other_id_3"
 	OtherID4                 IdentityType = "other_id_4"
+	OtherID5                 IdentityType = "other_id_5"
+	OtherID6                 IdentityType = "other_id_6"
+	OtherID7                 IdentityType = "other_id_7"
+	OtherID8                 IdentityType = "other_id_8"
+	OtherID9                 IdentityType = "other_id_9"
+	OtherID10                IdentityType = "other_id_10"
+	MobileNumber             IdentityType = "mobile_number"
+	PhoneNumber2             IdentityType = "phone_number_2"
+	PhoneNumber3             IdentityType = "phone_number_3"
 )
